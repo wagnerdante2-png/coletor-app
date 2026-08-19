602462fea2d51bac390c74b93a043109e3d8e2ac
wagnerdante2-png <wagnerdante2@gmail.com>
2026-08-19 10:20:39 -0300

    Wire dedicated inventory workflow

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import ReceivingBridge from './ReceivingBridge';
+import InventoryBridge from './InventoryBridge';
 import './styles.css';
 import './audit.css';
 import './module-grid.css';
@@ -9,10 +10,12 @@ import './validity.css';
 import './receiving.css';
 import './receiving-bridge.css';
 import './home-redesign.css';
+import './inventory.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <ReceivingBridge />
+    <InventoryBridge />
   </React.StrictMode>,
 );
