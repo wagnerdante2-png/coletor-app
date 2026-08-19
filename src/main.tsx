202bf16e4d9c03c615a5643eecf46825865717a6
wagnerdante2-png <wagnerdante2@gmail.com>
2026-08-19 09:50:23 -0300

    Wire receiving workflow into app

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ReceivingBridge from './ReceivingBridge';
 import './styles.css';
 import './audit.css';
 import './module-grid.css';
 import './validity.css';
+import './receiving.css';
+import './receiving-bridge.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <ReceivingBridge />
   </React.StrictMode>,
 );
